69b73180bf606f8d7d4c8d2a483e5c40e955d5ec
ahmtox <moreloshalan@gmail.com>
2023-04-21 20:31:34 -0400

    working audio, lights, and fsr

--- src/src.ino
+++ src/src.ino
@@ -1,20 +1,80 @@
 
 #include <Adafruit_NeoPixel.h>
+#include <SPI.h>
+#include <SD.h>
+#include <Adafruit_VS1053.h>
+//#include "protothreads.h"
 
 #ifdef __AVR__
  #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
 #endif
 
-#define LED_PIN 6
+#define VS1053_RESET   -1     // VS1053 reset pin (not used!)
+
+#define VS1053_CS       8     // VS1053 chip select pin (output)
+#define VS1053_DCS      10    // VS1053 Data/command select pin (output)
+#define CARDCS          7     // Card chip select pin
+#define VS1053_DREQ     9     // VS1053 Data request, ideally an Interrupt pin
+
+#define LED_PIN A1
 #define LED_COUNT 8
 
 int fsrPin = A0;     // the FSR and 10K pulldown are connected to a0
+int fsrPin2 = A2;     // the FSR and 10K pulldown are connected to a0
+int fsrPin3 = A3;     // the FSR and 10K pulldown are connected to a0
+int fsrPin4 = 24;     // the FSR and 10K pulldown are connected to a0
+int fsrPin5 = 25;     // the FSR and 10K pulldown are connected to a0
+int fsrPin6 = 13;     // the FSR and 10K pulldown are connected to a0
+
 int fsrReading;     // the analog reading from the FSR resistor divider
+int fsrReading2;     // the analog reading from the FSR resistor divider
+int fsrReading3;     // the analog reading from the FSR resistor divider
+int fsrReading4;     // the analog reading from the FSR resistor divider
+int fsrReading5;     // the analog reading from the FSR resistor divider
+int fsrReading6;     // the analog reading from the FSR resistor divider
+
+int marker = false;
 
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_VS1053_FilePlayer musicPlayer = Adafruit_VS1053_FilePlayer(VS1053_RESET, VS1053_CS, VS1053_DCS, VS1053_DREQ, CARDCS);
+
+//int playSound(struct pt* pt) {
+//  PT_BEGIN(pt);
+//
+//  while (true) {
+//    digitalWrite(
+//  }
+//}
  
 void setup(void) {
-  Serial.begin(9600);   
+  Serial.begin(9600);  
+  Serial1.begin(115200); 
+  while (!Serial1) { delay(1); }
+
+  if (!musicPlayer.begin()) { // initialise the music player
+     Serial1.println(F("Couldn't find VS1053, do you have the right pins defined?"));
+     while (1);
+  }
+  Serial1.println(F("VS1053 found"));
+  if (!SD.begin(CARDCS)) {
+    Serial1.println(F("SD failed, or not present"));
+    while (1);  // don't do anything more
+  }
+  Serial1.println("SD OK!");
+  //printDirectory(SD.open("/"), 0);
+
+  musicPlayer.setVolume(10,10);
+
+  #if defined(__AVR_ATmega32U4__) 
+    // Timer interrupts are not suggested, better to use DREQ interrupt!
+    // but we don't have them on the 32u4 feather...
+    musicPlayer.useInterrupt(VS1053_FILEPLAYER_TIMER0_INT); // timer int
+  #else
+    // If DREQ is on an interrupt pin we can do background
+    // audio playing
+    musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);  // DREQ int
+  #endif
+  
   #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
     clock_prescale_set(clock_div_1);
   #endif
@@ -25,17 +85,36 @@ void setup(void) {
  
 void loop(void) {
   fsrReading = analogRead(fsrPin);  
+  fsrReading2 = analogRead(fsrPin2);  
+  fsrReading3 = analogRead(fsrPin3);  
+  fsrReading4 = analogRead(fsrPin4);  
+  fsrReading5 = analogRead(fsrPin5);  
+  fsrReading6 = analogRead(fsrPin6);  
  
   Serial.print("Analog reading = ");
-  Serial.print(fsrReading);     // print the raw analog reading
+  Serial.print(fsrReading + " ");     // print the raw analog reading
+  Serial.print(fsrReading2 + " ");
+  Serial.print(fsrReading3 + " ");
+  Serial.print(fsrReading4 + " ");
+  Serial.print(fsrReading5 + " ");
+  Serial.print(fsrReading6);
  
   if (fsrReading < 10) {
     Serial.println(" - No pressure");
-  } else if (fsrReading < 200) {
+  } else if (fsrReading < 50) {
     Serial.println(" - Light touch");
-  } else if (fsrReading < 500) {
+  } else if (fsrReading < 120) {
     Serial.println(" - Light squeeze");
-  } else if (fsrReading < 800) {
+    if (!musicPlayer.playingMusic) {
+      musicPlayer.startPlayingFile("/track002.mp3");
+      colorWipe(strip.Color(  255, 0,   0), 10); // Green
+       colorWipe(strip.Color(  0, 0,   0), 10); // Green
+
+      delay(50);
+    } else {
+      musicPlayer.stopPlaying();
+    }
+  } else if (fsrReading < 250) {
     Serial.println(" - Medium squeeze");
   } else {
     Serial.println(" - Big squeeze");
@@ -57,9 +136,7 @@ void loop(void) {
 
 //  rainbow(10);             // Flowing rainbow cycle along the whole strip
 //  theaterChaseRainbow(50); // Rainbow-enhanced theaterChase variant
-
   
-  //delay(300);
 }
 
 void colorWipe(uint32_t color, int wait) {
@@ -140,3 +217,30 @@ void shockwave(uint32_t color, int wait) {
 //    }
 //  }
 //}
+
+
+/// File listing helper
+void printDirectory(File dir, int numTabs) {
+   while(true) {
+     
+     File entry =  dir.openNextFile();
+     if (! entry) {
+       // no more files
+       //Serial.println("**nomorefiles**");
+       break;
+     }
+     for (uint8_t i=0; i<numTabs; i++) {
+       Serial.print('\t');
+     }
+     Serial.print(entry.name());
+     if (entry.isDirectory()) {
+       Serial.println("/");
+       printDirectory(entry, numTabs+1);
+     } else {
+       // files have sizes, directories do not
+       Serial.print("\t\t");
+       Serial.println(entry.size(), DEC);
+     }
+     entry.close();
+   }
+}
